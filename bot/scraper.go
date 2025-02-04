952e4a0e7243e6fef82cc33866f90291bab6ff38
Ben Dorton <ben.dorton@smarty.com>
2025-02-04 11:00:53 -0700

    Refine headless browser bot

--- bot/scraper.go
+++ bot/scraper.go
@@ -2,49 +2,51 @@ package bot
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
-	"github.com/chromedp/cdproto/dom"
 	"github.com/chromedp/chromedp"
 )
 
-type Scraper struct {
+type HeadlessBrowser struct {
 	endpointURL string
 }
 
-func NewScraper(endpoint string) *Scraper {
-	return &Scraper{endpointURL: endpoint}
+func NewHeadlessBrowser(endpoint string) *HeadlessBrowser {
+	return &HeadlessBrowser{endpointURL: endpoint}
 }
 
-func (this *Scraper) Config() any {
+func (this *HeadlessBrowser) Config() any {
 	return this
 }
 
-func (this *Scraper) SendRequest() Response {
+func (this *HeadlessBrowser) SendRequest() Response {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	var html string
+	var responseBody string
 
-	err := chromedp.Run(ctx,
+	tasks := chromedp.Tasks{
 		chromedp.Navigate(this.endpointURL),
-		chromedp.Sleep(2*time.Second),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			rootNode, err := dom.GetDocument().Do(ctx)
-			if err != nil {
-				return err
-			}
-
-			html, err = dom.GetOuterHTML().WithNodeID(rootNode.NodeID).Do(ctx)
-			return err
-		}),
-	)
-	if err != nil {
-		return Response{
-			Body:  html,
-			Error: err,
-		}
+		chromedp.Sleep(3 * time.Second),
+		chromedp.OuterHTML("html", &responseBody),
 	}
 
-	return Response{Body: html}
+	err := chromedp.Run(ctx, tasks)
+	if err != nil {
+		return Response{Error: err}
+	}
+
+	if containsCloudflareChallenge(responseBody) {
+		return Response{StatusCode: 403, Body: "Blocked by Cloudflare", Error: fmt.Errorf("cloudflare challenge detected")}
+	}
+
+	return Response{StatusCode: 200, Body: responseBody, Error: nil}
+}
+
+func containsCloudflareChallenge(body string) bool {
+	return strings.Contains(body, "Checking your browser") ||
+		strings.Contains(body, "Please turn JavaScript on") ||
+		strings.Contains(body, "challenge-platform")
 }
